98ab671e53b56b4ec63e42059594d3ce66915764
Taja992 <Tharaxui@gmail.com>
2024-04-24 13:08:36 +0200

    Set up creating employees Added new exceptions Set up Connection manager

---
+++ src/main/BLL/EmployeeBLL.java
@@ -0,0 +1,18 @@
+package BLL;
+
+import BE.Employee;
+import DAL.EmployeeDAO;
+import Exceptions.BBExceptions;
+
+public class EmployeeBLL {
+    private final EmployeeDAO employeeDAO;
+
+    public EmployeeBLL() {
+        employeeDAO = new EmployeeDAO();
+    }
+
+
+    public void addNewEmployee(Employee employee) throws BBExceptions{
+        employeeDAO.newEmployee(employee);
+    }
+}
--- src/main/GUI/controller/AppController.java
+++ src/main/GUI/controller/AppController.java
@@ -1,4 +1,67 @@
 package GUI.controller;
 
+import BE.Employee;
+import Exceptions.BBExceptions;
+import GUI.model.EmployeeModel;
+import com.neovisionaries.i18n.CountryCode;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.math.BigDecimal;
+
 public class AppController {
+    @FXML
+    private TextField nameTxt;
+    @FXML
+    private TextField annualSalaryTxt;
+    @FXML
+    private TextField overheadMultiTxt;
+    @FXML
+    private TextField annualAmtTxt;
+    @FXML
+    private TextField yearlyHrsTxt;
+    @FXML
+    private TextField utilizationTxt;
+    @FXML
+    private CheckBox overheadChkBox;
+    @FXML
+    private ComboBox<String> countryCmbBox;
+
+    private final EmployeeModel employeeModel;
+
+    public AppController(){
+        employeeModel = new EmployeeModel();
+    }
+
+   public void initialize(){
+       populateCountryComboBox();
+   }
+
+   public void populateCountryComboBox() {
+        for (CountryCode code : CountryCode.values()) {
+            countryCmbBox.getItems().add(code.getName());
+        }
+   }
+
+    public void addEmployee(ActionEvent actionEvent) {
+       Employee employee = new Employee();
+       employee.setName(nameTxt.getText());
+       employee.setAnnualSalary(new BigDecimal(annualSalaryTxt.getText()));
+       employee.setOverheadMultiPercent(new BigDecimal(overheadMultiTxt.getText()));
+       employee.setAnnualAmount(new BigDecimal(annualAmtTxt.getText()));
+       employee.setCountry(countryCmbBox.getValue());
+       employee.setIsOverheadCost(overheadChkBox.isSelected());
+       employee.setWorkingHours(Integer.parseInt(yearlyHrsTxt.getText()));
+       employee.setUtilization(new BigDecimal(utilizationTxt.getText()));
+
+       try {
+           employeeModel.addNewEmployee(employee);
+       } catch (BBExceptions e){
+           e.printStackTrace();
+       }
+    }
 }
