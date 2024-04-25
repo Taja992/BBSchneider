1a1e9dc96632c36b7d447c667fc63da38d8e12d3
Taja992 <Tharaxui@gmail.com>
2024-04-25 15:00:52 +0200

    Put Overview tab into its own class

--- src/main/GUI/controller/AppController.java
+++ src/main/GUI/controller/AppController.java
@@ -2,16 +2,15 @@ package GUI.controller;
 
 import BE.Employee;
 import Exceptions.BBExceptions;
+import GUI.controller.tabs.OverviewTab;
 import GUI.model.EmployeeModel;
 import com.neovisionaries.i18n.CountryCode;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.math.BigDecimal;
-import java.text.NumberFormat;
 
 public class AppController {
     // --------Employee tab Label Fields--------
@@ -75,91 +74,15 @@ public class AppController {
         employeeModel = new EmployeeModel();
     }
 
-   public void initialize(){
+   public void initialize() {
+        //We pass all our FXML elements and employeeModel to the overviewTab class constructor
+       OverviewTab overviewTab = new OverviewTab(employeeModel, nameCol, annualSalaryCol, overHeadMultiCol, annualAmountCol, countryCol, teamCol, hoursCol, utilCol, overheadCol, overviewEmployeeTblView, employeeDayRateLbl, employeeHourlyRateLbl);
+       //Create our own initialize to easily call the methods in the class
+       overviewTab.initialize();
+
        populateCountryComboBox();
-       populateEmployeeTableView();
        populateEmployeeListView();
-
-       // Listener to the overview table view to calculate the rates
-       overviewEmployeeTblView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
-           if (newSelection != null) {
-               calculateEmployeeRates();
-           }
-       });
    }
-
-    public void populateEmployeeTableView() {
-        try {
-            // Get the list of employees from the model
-            ObservableList<Employee> employees = employeeModel.getEmployees();
-
-            // Populate the TableView
-            nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-            overHeadMultiCol.setCellValueFactory(new PropertyValueFactory<>("overheadMultiPercent"));
-            countryCol.setCellValueFactory(new PropertyValueFactory<>("country"));
-            teamCol.setCellValueFactory(new PropertyValueFactory<>("teamId"));
-            hoursCol.setCellValueFactory(new PropertyValueFactory<>("workingHours"));
-            //This method just adds dollar signs in front of the money values
-            //Once we add Conversion from USD to Euro this will probably need to be tweaked
-            addDollarSignsTableview();
-            //This method adds % signs to our tableview
-            addPercentSignsTableView();
-            overviewEmployeeTblView.setItems(employees);
-        } catch (BBExceptions e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void addDollarSignsTableview() {
-        NumberFormat format = NumberFormat.getNumberInstance();
-        //we set the format to have a minimum of 2 and a maximum of 2 digits after the decimal
-        format.setMinimumFractionDigits(2);
-        format.setMaximumFractionDigits(2);
-        //set up Annual Salary cell
-        annualSalaryCol.setCellValueFactory(new PropertyValueFactory<>("annualSalary"));
-        annualSalaryCol.setCellFactory(tc -> new TableCell<>() {
-            @Override
-            protected void updateItem(BigDecimal value, boolean empty) {
-                super.updateItem(value, empty);
-                //Here we use the NumberFormat class to format our numbers to have commas and add a $
-                setText(empty ? null : "$" + format.format(value));
-            }
-        });
-        //set up Annual amount cell the same way
-        annualAmountCol.setCellValueFactory(new PropertyValueFactory<>("annualAmount"));
-        annualAmountCol.setCellFactory(tc -> new TableCell<>() {
-            @Override
-            protected void updateItem(BigDecimal value, boolean empty) {
-                super.updateItem(value, empty);
-                setText(empty ? null : "$" + format.format(value));
-            }
-        });
-    }
-
-    public void addPercentSignsTableView() {
-        overHeadMultiCol.setCellValueFactory(new PropertyValueFactory<>("overheadMultiPercent"));
-        overHeadMultiCol.setCellFactory(tc -> new TableCell<>() {
-            @Override
-            protected void updateItem(BigDecimal value, boolean empty) {
-                super.updateItem(value, empty);
-                //This checks if cell is empty, if not continues..
-                //% is a placeholder for the value that will be inserted
-                //.2 this tells our tableview we want 2 digits after the decimal
-                //f indicates it's a floating point number (a number with a decimal)
-                //% we add this to the end of the number
-                setText(empty ? null : String.format("%.2f%%", value));
-            }
-        });
-
-        utilCol.setCellValueFactory(new PropertyValueFactory<>("utilization"));
-        utilCol.setCellFactory(tc -> new TableCell<>() {
-            @Override
-            protected void updateItem(BigDecimal value, boolean empty) {
-                super.updateItem(value, empty);
-                setText(empty ? null : String.format("%.2f%%", value));
-            }
-        });
-    }
 
     public void populateEmployeeListView() {
         try {
@@ -208,14 +131,6 @@ public class AppController {
        } catch (BBExceptions e){
            e.printStackTrace();
        }
-    }
-
-    public void calculateEmployeeRates() {
-        Employee selectedEmployee = overviewEmployeeTblView.getSelectionModel().getSelectedItem();
-        if(selectedEmployee != null){
-            employeeHourlyRateLbl.setText("$" + employeeModel.calculateHourlyRate(selectedEmployee) + "/Hr without overhead");
-            employeeDayRateLbl.setText("$" + employeeModel.calculateDailyRate(selectedEmployee) + "/Day without overhead");
-        }
     }
 
 }
