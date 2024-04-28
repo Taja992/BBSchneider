2300ff786eb303a2127dafaf78b4b19f160e1a8c
thomasbrowne0 <reapple119@gmail.com>
2024-04-28 17:00:40 +0200

    cleanup in employeeBLL

--- src/main/BLL/EmployeeBLL.java
+++ src/main/BLL/EmployeeBLL.java
@@ -24,12 +24,6 @@ public class EmployeeBLL {
     }
 
     public Double calculateHourlyRate(Employee selectedEmployee) {
-//        double annualSalary = selectedEmployee.getAnnualSalary().doubleValue();
-//        double workingHours = selectedEmployee.getWorkingHours();
-//        double hourlyRate = annualSalary / workingHours;
-//        // To find the hourly rate before the overhead multiplier was applied, divide the base hourly rate by 1 plus the multiplier
-//        double hourlyRateBeforeOverhead = hourlyRate / (1 + selectedEmployee.getOverheadMultiPercent().doubleValue());
-//        return Double.valueOf(String.format("%.2f", hourlyRateBeforeOverhead));
         double annualSalary = selectedEmployee.getAnnualSalary().doubleValue();
         double overheadMultiplier = selectedEmployee.getOverheadMultiPercent().doubleValue();
         double fixedAnnualAmount = selectedEmployee.getAnnualAmount().doubleValue();
@@ -39,13 +33,6 @@ public class EmployeeBLL {
     }
 
     public Double calculateDailyRate(Employee selectedEmployee) {
-//        double annualSalary = selectedEmployee.getAnnualSalary().doubleValue();
-//        double workingHours = selectedEmployee.getWorkingHours();
-//        double hourlyRate = annualSalary / workingHours;
-//        double hourlyRateBeforeOverhead = hourlyRate / (1 + selectedEmployee.getOverheadMultiPercent().doubleValue());
-//        // Multiplied by 8, assuming a day of work is 8 hours
-//        double dailyRateBeforeOverhead = hourlyRateBeforeOverhead * 8;
-//        return Double.valueOf(String.format("%.2f", dailyRateBeforeOverhead));
         double annualSalary = selectedEmployee.getAnnualSalary().doubleValue();
         double overheadMultiplier = selectedEmployee.getOverheadMultiPercent().doubleValue();
         double fixedAnnualAmount = selectedEmployee.getAnnualAmount().doubleValue();
