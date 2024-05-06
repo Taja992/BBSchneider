3e14427a3eb8ef878cfea5179c11a3526a71727a
thomasbrowne0 <reapple119@gmail.com>
2024-05-06 02:00:06 +0200

    created test class for employeebll to store calculations

--- src/main/UnitTest/EmployeeBLLTest.java
+++ src/main/UnitTest/EmployeeBLLTest.java
@@ -2,12 +2,16 @@ package UnitTest;
 
 import BE.Employee;
 import BLL.EmployeeBLL;
+
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
-public class OverviewTabTest {
-
+public class EmployeeBLLTest {
+    @Test
+    public void testCalculateEmployeeRates() {
+        
+    }
 }
