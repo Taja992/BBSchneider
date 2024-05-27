b68deaee2609b02689c773e307f1ea0ec0abd0bc
Taja992 <Tharaxui@gmail.com>
2024-05-27 11:19:19 +0200

    gave snapshot its own class

--- src/main/GUI/controller/AppController.java
+++ src/main/GUI/controller/AppController.java
@@ -103,6 +103,7 @@ public class AppController {
     private final TeamModel teamModel;
     private TeamTable teamTable;
     private SnapshotModel snapshotModel;
+    private SnapShotTable snapShotTable;
 
         //Coupling
     public AppController(){
@@ -122,6 +123,10 @@ public class AppController {
 
        this.teamTable.initialize();
 
+       this.snapShotTable = new SnapShotTable(snapshotModel, snapshotComboBox, snapshotHBox, teamTable, createSnapshotBtn);
+
+       this.snapShotTable.initialize();
+
 
        workingHoursTxt.setText(Integer.toString(8));
        employeeRatesListener();
@@ -136,7 +141,6 @@ public class AppController {
        workingHoursListener();
        selectTeamOnStart();
        selectFirstEmployee();
-       createTabsForSnapshots();
        populateComboBox();
        toolTips();
    }
@@ -153,152 +157,6 @@ public class AppController {
             grossMarginComboBox.getItems().add(i + "%");
         }
     }
-
-   public void CreateSnapshotFile(ActionEvent event) {
-
-        LocalDateTime currentDate = LocalDateTime.now();
-
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-
-        String newFileName = snapshotModel.createSnapshotFile("Snapshot on " + currentDate.format(format));
-
-        String tabName = newFileName.substring(12);
-        tabName = tabName.replace("-", "/");
-
-       snapshotComboBox.getItems().add(tabName);
-
-    }
-    // POPULATE COMBOBOX FROM HERE
-    //creates the tabs for each snapshot
-    private void createTabsForSnapshots(){
-        Map<String, String> allSnapshots = snapshotModel.getAllSnapshotNames();
-
-        // Assuming snapshotComboBox is your ComboBox
-        snapshotComboBox.getItems().clear(); // Clear existing items if any
-
-        for(String name : allSnapshots.keySet()){
-            snapshotComboBox.getItems().add(name);
-        }
-
-        // Set an action listener on the ComboBox
-        snapshotComboBox.setOnAction((event) -> {
-            String selectedSnapshot = snapshotComboBox.getSelectionModel().getSelectedItem();
-            if (selectedSnapshot != null) {
-                Map<String, String> snapshotList = snapshotModel.getAllSnapshotNames();
-
-                snapshotHBox.getChildren().clear();
-                TabPane snapTabPane = createTabPaneForSnapshot(snapshotList.get(selectedSnapshot));
-                snapshotHBox.getChildren().add(snapTabPane);
-                HBox.setHgrow(snapTabPane, Priority.ALWAYS);
-            }
-        });
-
-        orderSnapshotTabs();
-
-        // Select the first item by default
-        if (!snapshotComboBox.getItems().isEmpty()) {
-            snapshotComboBox.getSelectionModel().select(0);
-            String selectedSnapshot = snapshotComboBox.getSelectionModel().getSelectedItem();
-            if (selectedSnapshot != null) {
-                Map<String, String> snapshotList = snapshotModel.getAllSnapshotNames();
-
-                snapshotHBox.getChildren().clear();
-                TabPane snapTabPane = createTabPaneForSnapshot(snapshotList.get(selectedSnapshot));
-                snapshotHBox.getChildren().add(snapTabPane);
-                HBox.setHgrow(snapTabPane, Priority.ALWAYS);
-
-            }
-        }
-
-
-    }
-
-    //creates the content inside each snapshot tab (the tabpane including all the teams)
-    private TabPane createTabPaneForSnapshot(String filename){
-        TabPane snapTabPane = new TabPane();
-        snapTabPane.getStyleClass().add("teamTabPane");
-        snapTabPane.setMinWidth(826);
-        List<Team> teams = null;
-        try {
-            teams = snapshotModel.getAllTeamsInSnapshot(filename);
-        } catch (BBExceptions e) {
-            throw new RuntimeException(e);
-        }
-
-        for (Team team: teams){
-            Tab tab = new Tab(team.getName());
-            tab.setUserData(team);
-            tab.setClosable(false);
-            ObservableList<Employee> employeesInTeam = null;
-            try {
-                employeesInTeam = (ObservableList<Employee>) snapshotModel.getAllEmployeesFromTeam(team.getId(), filename);
-            } catch (BBExceptions e) {
-                throw new RuntimeException(e);
-            }
-            TableView<Employee> content = teamTable.createTableForTeam(team, employeesInTeam);
-            content.setEditable(false);
-
-            //to counteract the columns being editable (from the createTableForTeam() method)
-            TableColumn<Employee, Boolean> teamOverheadCol = (TableColumn<Employee, Boolean>) content.getColumns().get(7);
-            makeOverheadColumnNotEditable(teamOverheadCol);
-
-            tab.setContent(content);
-            snapTabPane.getTabs().add(tab);
-
-        }
-
-        return snapTabPane;
-    }
-
-    private void makeOverheadColumnNotEditable(TableColumn<Employee, Boolean> Col){
-        Col.setCellValueFactory(cellData -> new SimpleBooleanProperty(cellData.getValue().getTeamOverhead()));
-
-        Col.setCellFactory(column -> new TableCell<Employee, Boolean>() {
-            private final CheckBox checkBox = new CheckBox();
-            @Override
-            protected void updateItem(Boolean item, boolean empty) {
-                super.updateItem(item, empty);
-                if (empty) {
-                    setGraphic(null);
-                } else {
-
-                    Employee employee = getTableView().getItems().get(getIndex());
-                    checkBox.setSelected(employee.getTeamOverhead());
-                    checkBox.setDisable(true);
-                    setGraphic(checkBox);
-                }
-            }
-        });
-    }
-
-    private void orderSnapshotTabs(){
-        ObservableList<String> allItems = snapshotComboBox.getItems();
-
-        allItems.sort((item1, item2) ->{
-
-            String tab1Name = item1;
-            String tab2Name = item2;
-
-            //if either name contains "(2)" in case this is a duplicate file
-            if(tab1Name.contains("(") || tab2Name.contains("(")){
-                return tab1Name.compareTo(tab2Name); //compare them as strings if can't compare as date
-
-            } else {
-                DateTimeFormatter parser = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                LocalDate date1 = LocalDate.parse(tab1Name, parser);
-
-
-                LocalDate date2 = LocalDate.parse(tab2Name, parser);
-
-
-                return date1.compareTo(date2);
-            }
-
-
-        });
-
-    }
-
 
 
     ////////////////////////////////////////////////////////
